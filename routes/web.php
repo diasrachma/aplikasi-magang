9d729e8469a7a344e2aaa6fe151061fc5e1295af
diasrachma <diasrachma.dr@gmail.com>
2024-07-09 07:49:03 +0700

    up

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\PresensiController;
+
+Route::middleware(['guest:karyawan'])->group(function () {
+    Route::get('/', function () {
+        return view('auth.login');
+    })->name('login');
+
+    Route::post('/proseslogin', [AuthController::class, 'proseslogin']);
+});
+
+Route::middleware(['auth:karyawan'])->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index']);
+    Route::get('/proseslogout', [AuthController::class, 'proseslogout']);
+
+    //presensi
+    Route::get('presensi/create', [PresensiController::class, 'create']);
+    Route::post('presensi/store',[PresensiController::class, 'store']);
+});
